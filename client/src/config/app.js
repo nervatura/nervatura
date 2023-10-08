7152ba83992da85b13274930c49688c19277b628
nervatura <kappel.csaba@gmail.com>
2023-10-08 20:05:16 +0300

    v5.2.1

--- client/src/config/app.js
+++ client/src/config/app.js
@@ -18,7 +18,7 @@ export const store = {
     serverURL,
     apiPath: "/api",
     engines: ["sqlite", "sqlite3", "mysql", "postgres", "mssql"],
-    service: ["dev", "5.1.8", "5.1.9", "5.2.0"],
+    service: ["dev", "5.1.9", "5.2.0", "5.2.1"],
     helpPage: "https://nervatura.github.io/nervatura/docs/client/"
   },
   ui: {
